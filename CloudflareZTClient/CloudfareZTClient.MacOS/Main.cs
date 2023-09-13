ece242349557ab4eec9011e84274de75996259e8
Vlad <vlad.samonin@librestream.com>
2023-09-13 13:23:34 -0500

    Adding Mac Project

---
+++ CloudflareZTClient/CloudfareZTClient.MacOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+using DarkModeSample.MacOS;
+
+namespace CloudfareZTClient.MacOS
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			NSApplication.Main (args);
+		}
+	}
+}
